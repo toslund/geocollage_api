88758f508ae24d7843149e0b9484255955a636cc
Tim Oslund <to048293@gmail.com>
2019-10-23 15:39:00 -0700

    add alembic

---
+++ alembic/versions/e2ea6d972b73_baseline.py
@@ -0,0 +1,24 @@
+"""baseline
+
+Revision ID: e2ea6d972b73
+Revises: 
+Create Date: 2019-10-23 15:15:57.418468
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e2ea6d972b73'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
